927c58f9c3553ea5199168ffaa29d78e7a240436
Dahee MATTANA <kim01091@gmail.com>
2025-08-18 15:28:47 +0200

    whitespace correction for CI

--- platform/airflow/dags/healthcheck.py
+++ platform/airflow/dags/healthcheck.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime
-with DAG(
-    'healthcheck'
-    , start_date = datetime(2025, 1, 1)
-    , schedule = None
-    , catchup = False
+with DAG( \
+    'healthcheck' \
+    , start_date = datetime(2025, 1, 1) \
+    , schedule = None \
+    , catchup = False \
 ) : EmptyOperator(task_id = 'up')
