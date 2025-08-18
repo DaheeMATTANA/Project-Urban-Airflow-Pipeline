386a952265e134860e0f462bd6635a42d61cfed1
Dahee MATTANA <kim01091@gmail.com>
2025-08-18 15:33:40 +0200

    Correction for linter

--- platform/airflow/dags/healthcheck.py
+++ platform/airflow/dags/healthcheck.py
@@ -1,9 +1,7 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime
-with DAG( \
-    'healthcheck' \
-    , start_date = datetime(2025, 1, 1) \
-    , schedule = None \
-    , catchup = False \
-) : EmptyOperator(task_id = 'up')
+with DAG('healthcheck',
+         start_date=datetime(2025, 1, 1),
+         schedule=None,
+         catchup=False): EmptyOperator(task_id='up')
