ba80c7abce2219bf1a7b94b5197eb2bc621e70f5
DaheeMATTANA <kim01091@gmail.com>
2025-08-23 17:26:13 +0200

    add gbfs_stream DAG and python package

---
+++ infra_platform/airflow/dags/gbfs_stream.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from infra_platform.pipelines.streaming.gbfs_producer import produce
+
+# DAG
+default_args = {
+    "owner": "data_engineer",
+    "retries": 1,
+    "retry_delay": timedelta(seconds=30),
+}
+
+with DAG(
+    dag_id="gbfs_stream",
+    default_args=default_args,
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="* * * * *",  # every 1 minute
+    catchup=False,
+    tags=["gbfs", "streaming"],
+) as dag:
+    produce_task = PythonOperator(
+        task_id="produce_gbfs_station_status",
+        python_callable=produce,
+    )
