ab4aaf53d5c47cfe44962ef26aea5f223895d6f8
DaheeMATTANA <kim01091@gmail.com>
2025-08-24 18:33:10 +0200

    add duckdb loader for gbfs

--- infra_platform/airflow/dags/gbfs_consumer_dag.py
+++ infra_platform/airflow/dags/gbfs_consumer_dag.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from pipelines.ingestion.gbfs_consumer_batch import consume_batch
+from pipelines.loading.gbfs_duckdb_loader import load_gbfs_to_duckdb
 
 with DAG(
     dag_id="gbfs_consumer_dag",
@@ -10,7 +11,7 @@ with DAG(
     schedule_interval="*/5 * * * *",
     catchup=False,
 ) as dag:
-    t1 = PythonOperator(
+    consume_task = PythonOperator(
         task_id="consume_gbfs_to_minio_batch",
         python_callable=consume_batch,
         op_kwargs={
@@ -18,3 +19,14 @@ with DAG(
             "timeout_s": 30,
         },
     )
+
+    # Optional loader task - can be enabled/disabled
+    load_to_duckdb_task = PythonOperator(
+        task_id="load_gbfs_to_duckdb",
+        python_callable=load_gbfs_to_duckdb,
+        op_kwargs={
+            "date_filter": "{{ ds }}"  # Use Airflow execution date
+        },
+    )
+
+    consume_task >> load_to_duckdb_task
--- infra_platform/setup.py
+++ infra_platform/setup.py
@@ -4,5 +4,12 @@ setup(
     name="infra_platform",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["kafka-python", "requests", "pytz", "minio"],
+    install_requires=[
+        "kafka-python",
+        "requests",
+        "pytz",
+        "minio",
+        "duckdb",
+        "pandas",
+    ],
 )
