36a03d11407c54740c255fb55a1797dd6457af41
DaheeMATTANA <kim01091@gmail.com>
2025-10-09 17:15:24 +0200

    add stg_gbfs_station_information

---
+++ analytics/dbt/urban_airflow_analytics/models/staging/stg_gbfs_station_information.sql
@@ -0,0 +1,36 @@
+WITH 
+
+source_gbfs_station_status AS (
+    SELECT *
+    FROM {{ source('raw', 'raw_gbfs_station_information') }}
+)
+
+, renamed AS (
+    SELECT
+        station_id
+        , stationCode AS station_code
+        , name AS station_name
+        , lat AS latitude
+        , lon AS longitude
+        , capacity
+        , station_opening_hours
+        , rental_methods
+        , ingestion_date AS part_date_utc
+        , ingestion_hour AS part_hour_utc
+        , created_at AS loaded_at_utc
+    FROM source_gbfs_station_status
+)
+
+SELECT
+    station_id
+    , station_code
+    , station_name
+    , latitude
+    , longitude
+    , capacity
+    , station_opening_hours
+    , rental_methods
+    , part_date_utc
+    , part_hour_utc
+    , loaded_at_utc
+FROM renamed
