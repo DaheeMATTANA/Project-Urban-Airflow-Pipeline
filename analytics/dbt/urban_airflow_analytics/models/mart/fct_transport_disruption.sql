7dc99fb29c9a1fb8e4144b150981d30ee57e464f
DaheeMATTANA <kim01091@gmail.com>
2025-11-09 16:46:33 +0100

    add fct_transport_disruption

---
+++ analytics/dbt/urban_airflow_analytics/models/mart/fct_transport_disruption.sql
@@ -0,0 +1,24 @@
+WITH
+
+transport_disruption AS (
+    SELECT *
+    FROM
+        {{ ref('int_transport_disruption_flagged') }}
+    WHERE 1 = 1
+    -- filter by the date of pipeline stabilisation
+    AND ended_at_cet >= '2025-09-29'
+)
+
+SELECT
+    event_id
+    , started_at_cet
+    , ended_at_cet
+    , disruption_cause_fr
+    , disruption_severity_fr
+    , message_title_fr
+    , message_body_fr
+    , disruption_duration
+    , duration_category
+    , is_active
+    , is_planned
+FROM transport_disruption
