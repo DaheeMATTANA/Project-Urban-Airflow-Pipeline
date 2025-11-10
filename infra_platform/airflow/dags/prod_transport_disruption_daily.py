26bea657ca3edeb85edb7469692948acce19423c
DaheeMATTANA <kim01091@gmail.com>
2025-11-10 16:24:41 +0100

    add prod dags

--- infra_platform/airflow/dags/prod_transport_disruption_daily.py
+++ infra_platform/airflow/dags/prod_transport_disruption_daily.py
@@ -5,11 +5,11 @@ from dags.common.defaults import DEFAULT_ARGS
 from pipelines.loading.dbt_artifacts_duckdb_loader import load_dbt_test_results
 
 """
-## (PROD) Build fct_hourly_station_status model DAG
+## (PROD) Build fct_daily_transport_disruption model DAG
 
-This DAG runs every week to refresh fct_station_status in prod.
+This DAG runs every week to refresh fct_daily_transport_disruption in prod.
     - runs all the upstream models
-    - every Monday at 4:30AM UTC.
+    - every Monday at 5:00AM UTC.
     - Logs dbt test results into a materialised table.
 
 Owner: Team Buldo
@@ -20,13 +20,13 @@ DBT_PROJECT_DIR = "/opt/airflow/src/analytics/dbt/urban_airflow_analytics"
 DBT_PROFILES_DIR = "/opt/airflow/dbt"
 
 with DAG(
-    dag_id="prod_station_status_daily",
+    dag_id="prod_transport_disruption_daily",
     default_args=DEFAULT_ARGS,
-    schedule_interval="30 4 * * *",  # At 04:30
+    schedule_interval="0 5 * * *",  # At 05:00
     max_active_runs=1,
     max_active_tasks=2,
     concurrency=1,
-    tags=["dbt", "fct", "station_status", "prod"],
+    tags=["dbt", "fct", "transport_disruption", "prod"],
     doc_md=__doc__,
 ) as dag:
     dbt_build = BashOperator(
@@ -35,7 +35,7 @@ with DAG(
             f"dbt build "
             f"--project-dir {DBT_PROJECT_DIR} "
             f"--target prod "
-            f"--select +fct_hourly_station_status "
+            f"--select +fct_daily_transport_disruption "
             f"--exclude models/staging"
         ),
         env={
